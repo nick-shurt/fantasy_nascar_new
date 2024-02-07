365b42e00c438ae3ea72aec8cbfe18e74d7d7a3a
Nicholas Shurtleff <nicholas.shurtleff@higherechelon.com>
2024-02-07 13:25:41 -0500

    Updating sportradar api key

--- nascar_results_2023.php
+++ nascar_results_2023.php
@@ -31,7 +31,7 @@ $rd_day = $race_date->format('d');
 $td_day = $today_date->format('d');
 
 //if ($rd_month == $td_month && $rd_day == $td_day) {
-    $key = "mmhgpef37j537ahyz45ad33a";
+    $key = "ruaqqyhzz57dqc36e228qkgm";
     $request = "http://api.sportradar.us/nascar-ot3/mc/races/" . $raceId . "/results.xml?api_key=" . $key;
     //$request = "";
 
