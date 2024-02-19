fd55e28bf9b59974e6a5a0b62f5991daa6872620
Nicholas Shurtleff <nicholas.shurtleff@higherechelon.com>
2024-02-19 14:05:07 -0500

    Updating teams file bc Im an idiot

--- nascar_drivers_teams_2024.php
+++ nascar_drivers_teams_2024.php
@@ -1,6 +1,6 @@
 <?php
 
-$team_matt = new Team("Team Matt", "Martin Truex Jr", "Alex Bowman", "Josh Berry");
+$team_matt = new Team("Team Matt", "Martin Truex Jr", "Alex Bowman", "Josh Berry", "Josh Williams");
 $team_nick = new Team("Team Nick", "William Byron", "Tyler Reddick", "Corey Lajoie", "Shane Van Gisbergen");
 $team_donna = new Team("Team Donna", "Kyle Busch", "Erik Jones", "Daniel Suarez", "Kaz Grala");
 $team_joey = new Team("Team Joey", "Ross Chastain", "Chris Buescher", "Todd Gilliland", "Cody Ware");
@@ -9,7 +9,7 @@ $team_chives = new Team("Team Chives", "Joey Logano", "Chase Briscoe", "Austin D
 $team_jim = new Team("Team Jim", "Denny Hamlin", "Bubba Wallace", "Noah Gragson", "AJ Allmendinger");
 $team_mike = new Team("Team Mike", "Kyle Larson", "Ty Gibbs", "Carson Hocevar", "Zane Smith");
 $team_jru = new Team("Team Jru", "Ryan Blaney", "Michael McDowell", "Ricky Stenhouse Jr", "Justin Haley");
-$team_docks = new Team("Team Docks", "Chase Elliott", "John H. Nemechek", "Ryan Preece");
+$team_docks = new Team("Team Docks", "Chase Elliott", "John H. Nemechek", "Ryan Preece", "Harrison Burton");
 
 $team_standings = array($team_matt,$team_nick,$team_donna,$team_joey,$team_rachel,$team_chives,$team_jim,$team_mike,$team_jru,$team_docks);
 $team_roster = $team_standings;
