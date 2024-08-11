96995b313b2fe6ad4b8e37f6292a98c9b33c82dd
nick-shurt <47427008+nick-shurt@users.noreply.github.com>
2024-08-11 13:29:55 -0400

    Updating api key

--- nascar_results_2024.php
+++ nascar_results_2024.php
@@ -31,7 +31,7 @@ $rd_day = $race_date->format('d');
 $td_day = $today_date->format('d');
 
 //if ($rd_month == $td_month && $rd_day == $td_day) {
-    $key = "IFNXBPl4WI66uhuJYmxZaaySuKH4y43j9w45poA9";
+    $key = "J6xKq1plKcacK0Pin2VgNanXiPjXGk8v4PV1U5mg";
     $request = "http://api.sportradar.us/nascar-ot3/mc/races/" . $raceId . "/results.xml?api_key=" . $key;
     //$request = "";
 
