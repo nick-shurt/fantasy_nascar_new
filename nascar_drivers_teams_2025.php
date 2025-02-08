48c8c1e91462f8e261af593f03928e9b74eda4d2
Nicholas Shurtleff <nicholas.shurtleff@higherechelon.com>
2025-02-08 01:42:50 -0500

    Starting process for 2025 season

---
+++ nascar_drivers_teams_2025.php
@@ -0,0 +1,19 @@
+<?php
+
+$team_rachel = new Team("Team Rachel", "Denny Hamlin", "Carson Hocevar", "Ryan Preece", "Zane Smith");
+$team_mike = new Team("Team Mike", "Ryan Blaney", "Daniel Suarez", "Erik Jones", "Austin Dillon");
+$team_donna = new Team("Team Donna", "Tyler Reddick", "Chase Briscoe", "AJ Allmendinger", "Martin Truex Jr");
+$team_jim = new Team("Team Jim", "William Byron", "Austin Cindric", "Justin Haley", "Corey Lajoie");
+$team_joey = new Team("Team Joey", "Alex Bowman", "Kyle Busch", "Ricky Stenhouse Jr", "Connor Zilisch");
+$team_steve = new Team("Team Docks", "Kyle Larson", "Michael McDowell", "Shane Van Gisbergen", "Cole Custer");
+$team_nick = new Team("Team Nick", "Chris Buescher", "Brad Keselowski", "Todd Gilliland", "Cody Ware");
+$team_matt = new Team("Team Matt", "Christopher Bell", "Bubba Wallace", "Josh Berry", "Justin Allgaier");
+$team_chives = new Team("Team Chives", "Chase Elliott", "Ross Chastain", "Riley Herbst", "Ty Dillon");
+$team_jru = new Team("Team Jru", "Joey Logano", "Ty Gibbs", "Noah Gragson", "John H. Nemechek");
+
+$week_1_teams = array($team_rachel,$team_mike,$team_donna,$team_jim,$team_joey,$team_steve,$team_nick,$team_matt,$team_chives,$team_jru);
+$team_standings = array($team_rachel,$team_mike,$team_donna,$team_jim,$team_joey,$team_steve,$team_nick,$team_matt,$team_chives,$team_jru);
+$team_roster = $team_standings;
+$driver_rank = array();
+
+?>
